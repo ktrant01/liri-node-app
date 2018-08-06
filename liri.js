b3881bae88b4d9e80b019d01316a6edf6e41ebb8
ktrantham <ktrantham@diplomat.is>
2018-08-05 21:39:00 -0700

    final, but with limited OMDB API functionality

--- liri.js
+++ liri.js
@@ -1,13 +1,31 @@
 require("dotenv").config();
 
 
+
+var Spotify = require('node-spotify-api');
  
+var spotify = new Spotify({
+  id: "5984c743cb974d0299f1ebb9b7c5216b",
+  secret: "28219596b6284fa8bcb5120ec60cc22a"
+});
+ 
+
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+   
+//   console.log(data.tracks.items[0].album.artists[0].name); 
+//   console.log(data.tracks.items[0].name); 
+//   console.log(data.tracks.items[0].album.name); 
+//   });
+
+ var OMDB_API_KEY="c43a8b6d"
+
+
 var omdbApi = require('omdb-client');
 
-var spotify = require('spotify');
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
 
 
 
@@ -31,17 +49,27 @@ if (process.argv[2] == "movie-this"){
 
 
 if (process.argv[2] == "spotify-this-song"){
-    spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-        if ( err ) {
-            console.log('Error occurred: ' + err);
-            return;
+    spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
         }
-     
-       console.log(data)
-    });
+       
+      console.log("Artist:       " + data.tracks.items[0].album.artists[0].name); 
+      console.log("Song Name:    " + data.tracks.items[0].name); 
+      console.log("Album name:   " + data.tracks.items[0].album.name); 
+      console.log("Spotify URL:  " + data.tracks.items[0].album.external_urls.spotify); 
+      });
 }
 
 
- 
+// * Artist(s)
+     
+// * The song's name
+
+// * A preview link of the song from Spotify
+
+// * The album that the song is from
+
+
 
 
